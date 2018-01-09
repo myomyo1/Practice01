88f0ecb4e2ccb1a1bf2d321f56fb155c6eb108b9
bit-user <bit-user@DESKTOP-C1QR4BV>
2018-01-09 13:07:45 +0900

    poblem05 추가

--- src/practice02/Problem05.java
+++ src/practice02/Problem05.java
@@ -1,14 +1,37 @@
 package practice02;
 
+import java.util.HashSet;
+
 public class Problem05 {
 	public static void main(String[] args) {
 
 		int num_array[] = new int[6];
-
-		for (int i = 0; i < num_array.length; i++) {
-			int random_number = (int) ((Math.random() * 45) + 1);
-			num_array[i] = random_number;
-			System.out.print(num_array[i]+ " ");
+		int count=0;
+		while(count<6) {
+		getlottonum : for(int i = 0; i < num_array.length; i++) {  //6번돌릴꺼
+			int random_number = (int) ((Math.random() * 45) + 1); //랜덤값random_number에 저장 
+			for(int j=0; j<i; j++) {
+				if(num_array[j]==random_number) {
+					continue getlottonum;
+				}
+			}
+			num_array[i]=random_number;//랜덤값 배열에 저장
+			System.out.println(num_array[i]);
+			count++;
 		}
-	}
+		}	
+			
+	
+		
+//		HashSet<Integer> set = new HashSet<>(); //set만들고
+//		while(set.size()<6) { //6넘어가면 while문 밖으로
+//			int j=(int) ((Math.random() * 45) + 1); //랜덤
+//			set.add(j);
+//		}
+//		for(int lotto : set) {
+//			System.out.print(lotto+ " ");
+//		}
+		
+		
+}		
 }
